6289ff0dd420373abad6ac9927302ccfe76148e8
Muthu Venkatachalam <muthukarupasamy.venkatachalam@resideo.com>
2020-02-12 21:12:18 +0530

    styled login page

--- frontend/src/pages/authPage.js
+++ frontend/src/pages/authPage.js
@@ -1,9 +1,25 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import './Auth.css'
 
 class AuthPage extends Component {
-    render(){
-      return <h1>Auth Page</h1>
-    }
+  render() {
+    return (
+      <form className="auth-form">
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="form-actions">
+          <button type="button">SignUp</button>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    );
   }
-  
-export default AuthPage
+}
+
+export default AuthPage;
